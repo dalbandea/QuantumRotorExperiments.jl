72af4607f28ae4c6fcd2c5980a9441c76682fc21
David Albandea PROX15-2 <albandeadavid@gmail.com>
2024-01-17 15:15:52 +0100

    Add differentiable local susceptibility and ZeroMode simulations
    ZeroMode aux field simulations set phi[1] = 0 always (and mom[1] =
    frc[1] = 0) with the intention of making <phi[t]> = 0 for every t

--- src/QuantumRotorExperiments.jl
+++ src/QuantumRotorExperiments.jl
@@ -5,7 +5,7 @@ import LFTSampling: metropolis_accept_reject!
 import LFTQuantumRotor
 import BDIO
 import BDIO: BDIO_write!, BDIO_read
-import FormalSeries
+using FormalSeries
 import ADerrors
 import ADerrors: uwreal
 import LinearAlgebra
@@ -13,13 +13,18 @@ import LinearAlgebra
 include("qrio.jl")
 # export BDIO_write!, BDIO_read
 include("qrobservables.jl")
-export compute_q2, compute_q, diff_top_charge
+export compute_q2, compute_q, diff_top_charge, ZeroMode
 include("qraderrors.jl")
 include("trivmap.jl")
 
+include("qrseries.jl")
+export restart_derivatives, restart_derivatives!
+export Mod, Mod!
+export diff_local_susceptibility
+
 
 function LFTSampling.metropolis_accept_reject!(lftws::L, lftcp::L, dS::FormalSeries.Series) where {L <: LFTQuantumRotor.QuantumRotor} 
-    println(dS)
+    # println(dS)
 end
 
 end # module QuantumRotorExperiments
--- src/qrobservables.jl
+++ src/qrobservables.jl
@@ -10,3 +10,36 @@ function compute_q(qrws::LFTQuantumRotor.QuantumRotor, disc::Type{D}) where D <:
 end
 
 
+
+diff_local_susceptibility(qrws::LFTQuantumRotor.QuantumRotor) = diff_local_susceptibility(qrws, qrws.params.disc, qrws.params.BC)
+function diff_local_susceptibility(qrws::LFTQuantumRotor.QuantumRotor, disc::Type{LFTQuantumRotor.StandardDiscretization}, ::Type{BC}) where BC <: LFTQuantumRotor.AbstractBoundaryCondition
+    chi = zero(eltype(qrws.phi))
+    for i in 1:qrws.params.iT-1
+        chi += sin(qrws.phi[i+1]-qrws.phi[i])^2
+    end
+
+    if BC == LFTQuantumRotor.PeriodicBC
+        chi += sin(qrws.phi[1]-qrws.phi[qrws.params.iT])^2
+    end
+
+    return chi/(2pi)^2/(qrws.params.iT-1)
+end
+
+import LFTQuantumRotor: AbstractAuxFields
+
+struct ZeroMode <: AbstractAuxFields end
+
+import LFTQuantumRotor: left_boundary_force!, update_fields!
+
+function left_boundary_force!(qrws::QuantumRotor, hmcws::LFTQuantumRotor.QuantumRotorHMC, disc::Type{LFTQuantumRotor.StandardDiscretization}, BC::Type{LFTQuantumRotor.OpenBC}, aux::ZeroMode)
+    hmcws.frc[1] = 0.0
+    println("hi")
+    return nothing
+end
+
+function update_fields!(qrws::QuantumRotor, epsilon, hmcws::LFTQuantumRotor.QuantumRotorHMC, aux::ZeroMode)
+    # Update phi field
+    qrws.phi .= qrws.phi .+ epsilon .* hmcws.mom ./ hmcws.params.width^2
+    qrws.phi[1] = 0.0
+    return nothing
+end
